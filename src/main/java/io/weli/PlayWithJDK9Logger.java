409c88d728f65e029266d471d9b03a92f8d3c57a
Weinan Li <l.weinan@gmail.com>
2018-04-19 16:23:33 +0800

    .

--- src/main/java/io/weli/PlayWithJDK9Logger.java
+++ src/main/java/io/weli/PlayWithJDK9Logger.java
@@ -2,6 +2,7 @@ package io.weli;
 
 public class PlayWithJDK9Logger {
 
+    // https://www.safaribooksonline.com/library/view/java-9-new/9781786464637/video3_3.html
     public static void main(String[] args) throws Exception {
 
     }
