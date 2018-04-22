76579a4ecd5cb9f3114de79cd74338f65c213a74
Weinan Li <l.weinan@gmail.com>
2018-04-22 20:32:44 +0800

    .

---
+++ src/main/java/io/weli/MyLogger.java
@@ -0,0 +1,27 @@
+package io.weli;
+
+import java.io.Serializable;
+
+public final class MyLogger extends java.util.logging.Logger implements Serializable {
+
+    /**
+     * Protected method to construct a logger for a named subsystem.
+     * <p>
+     * The logger will be initially configured with a null Level
+     * and with useParentHandlers set to true.
+     *
+     * @param name               A name for the logger.  This should
+     *                           be a dot-separated name and should normally
+     *                           be based on the package name or class name
+     *                           of the subsystem, such as java.net
+     *                           or javax.swing.  It may be null for anonymous Loggers.
+     * @param resourceBundleName name of ResourceBundle to be used for localizing
+     *                           messages for this logger.  May be null if none
+     *                           of the messages require localization.
+     * @throws MissingResourceException if the resourceBundleName is non-null and
+     *                                  no corresponding resource can be found.
+     */
+    protected MyLogger(String name, String resourceBundleName) {
+        super(name, resourceBundleName);
+    }
+}
